91808f97a1952b0f379cd045c1fe7e6c9bd1a3e3
Resta Adiputra <resta.adiputra@gmail.com>
2020-09-03 10:30:16 +0700

    add server env variable

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = () => {
   return axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: process.env.REACT_APP_SERVER_API,
     data: [],
   });
 };
